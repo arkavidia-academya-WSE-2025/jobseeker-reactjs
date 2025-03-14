3cc8dd6695f1114890f0f21f3288d9b84a008d6b
xturus138 <radityaramadhan.138@gmail.com>
2025-03-14 22:02:26 +0700

    Update: Home component to render a new div with specific classes and content

--- fe-arkavidia/src/components/pages/home/Home.jsx
+++ fe-arkavidia/src/components/pages/home/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-full space-y-16 pb-16">This is the home section</div>
+  );
 };
 
 export default Home;
