6d490a5d05b1bab5b2e6260a14f4a6587af0514d
Raditya Ramadhan <156584367+xturus138@users.noreply.github.com>
2025-03-14 21:49:08 +0700

    Revert "feat: change all the main feature and structure folder"

--- fe-arkavidia/src/App.jsx
+++ fe-arkavidia/src/App.jsx
@@ -1,21 +1,11 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <Router>
-        <main className="w-full min-h-screen flex flex-col bg-neutral-50 text-neutral-500">
-          {/* Navbar Section */}
-          <Navbar />
-
-          {/* Routing */}
-
-          {/* Footer Section */}
-          <Footer />
-        </main>
-      </Router>
+      <h1 className="text-3xl font-bold underline">
+        Hello world! Radit Branch
+      </h1>
     </>
   );
 }
