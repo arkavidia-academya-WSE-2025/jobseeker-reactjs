2c88b18aa0be9e45c68ed8828179fb64a83ab577
xturus138 <radityaramadhan.138@gmail.com>
2025-03-14 22:56:20 +0700

    feat: Added search bar and trending topics section to Hero component

--- fe-arkavidia/src/components/pages/home/hero/Hero.jsx
+++ fe-arkavidia/src/components/pages/home/hero/Hero.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Layout from "../../../layout/Layout";
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+import { MdTrendingUp } from "react-icons/md";
 
 const Hero = () => {
   return (
@@ -18,9 +21,55 @@ const Hero = () => {
         <div className="space-y-4 w-full">
           {/*Search Section */}
           <div className="w-full p-3.5 flex items-center justify-between gap-5 flex-wrap bg-sky-500/5 md:rounded-full rounded-xl border border-sky-500/50 backdrop-blur">
-            Search for Jobs
+            <div className="flex-1">
+              <input
+                type="text"
+                placeholder="Search jobs, Companies..."
+                className="w-full px-4 py-3 rounded-full text-base text-neutral-50 font-normal bg-transparent backdrop-blur-lg border border-neutral-50/40
+                outline-none focus:border-sky-500/40 focus:outline-none ease-in-out duration-300"
+              />
+            </div>
+            <button
+              className="md:w-fit w-full px-5 py-3 rounded-full text-base text-neutral-900 font-medium
+             bg-sky-500 hover:bg-sky-600 flex items-center justify-center gap-x-2 ease-in-out duration-300"
+            >
+              <FaSearch />
+              Search Jobs
+            </button>
           </div>
           {/*Trending Topic Section */}
+          <div className="w-full inline-flex items-center gap-x-2">
+            <div className="flex items-center gap-x-1 5">
+              <MdTrendingUp className="text-lg text-sky-500" />
+              <p className="text-sm text-neutral-300">Top Trending Jobs:</p>
+            </div>
+
+            <div className="flex items-center gap-x-2 flex-wrap">
+              <div>
+                <Link className="text-sm font-normal text-neutral-400 hover:text-sky-500 ease-in-out duration-300">
+                  UI/Ux
+                </Link>
+                ,
+              </div>
+              <div>
+                <Link className="text-sm font-normal text-neutral-400 hover:text-sky-500 ease-in-out duration-300">
+                  Front-End Developer
+                </Link>
+                ,
+              </div>
+              <div>
+                <Link className="text-sm font-normal text-neutral-400 hover:text-sky-500 ease-in-out duration-300">
+                  Back-End Developer
+                </Link>
+                ,
+              </div>
+              <div>
+                <Link className="text-sm font-normal text-neutral-400 hover:text-sky-500 ease-in-out duration-300">
+                  Android Developer
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
