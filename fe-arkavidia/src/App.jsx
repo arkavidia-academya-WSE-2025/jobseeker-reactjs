e1c75728d5d272c712c98745863744a9a4466fbb
izzahfaiqputri <izzahfaiqputri@gmail.com>
2025-03-18 11:07:58 +0700

    adding Profile Page with a little bit error

--- fe-arkavidia/src/App.jsx
+++ fe-arkavidia/src/App.jsx
@@ -5,6 +5,9 @@ import Home from "./components/pages/home/Home";
 import Chat from "./components/pages/chat/Chat";
 import NotificationPage from "./components/pages/notifications/NotificationPage";
 import JobSearch from "./components/pages/home/jobs/JobSearch";
+import LoginPage from "./components/pages/auth/LoginPage";
+import SignUpPage from "./components/pages/auth/SignUpPage";
+import ProfilePage from "./components/pages/ProfilePage";
 
 function App() {
   return (
@@ -16,10 +19,13 @@ function App() {
 
           {/* Routing */}
           <Routes>
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/" element={<Home />} />
             <Route path="/chat" element={<Chat />} />
             <Route path="/notification" element={<NotificationPage />} />
             <Route path="/findJobs" element={<JobSearch />} />
+            <Route path="/profile:/username" element={<ProfilePage />} />
           </Routes>
 
           {/* Footer Section */}
