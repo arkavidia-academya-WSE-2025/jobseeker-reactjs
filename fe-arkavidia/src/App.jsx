ef98f09e24f19e84965213cd8fb34c6e87cd1870
xturus138 <radityaramadhan.138@gmail.com>
2025-03-22 06:38:31 +0700

    Refactor: App component to use Layout component and update import statements

--- fe-arkavidia/src/App.jsx
+++ fe-arkavidia/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./components/pages/home/Home";
@@ -17,31 +22,43 @@ import PrivateRoute from "./components/context/PrivateRoute";
 import RecruiterRoute from "./components/context/RecruiterRoute";
 import ChatJobSeeker from "./components/pages/chat/ChatJobSeeker";
 
+const Layout = ({ children }) => {
+  const location = useLocation();
+  const hideLayout = ["/login", "/register"].includes(location.pathname);
+  return (
+    <>
+      {!hideLayout && <Navbar />}
+      {children}
+      {!hideLayout && <Footer />}
+    </>
+  );
+};
+
 function App() {
   return (
     <Router>
-      <main className="w-full min-h-screen flex flex-col bg-neutral-50 text-neutral-500">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<SignUpPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route element={<PrivateRoute />}>
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/editProfile" element={<EditProfile />} />
-            <Route path="/chat" element={<Chat />} />
-            <Route path="/chatJob" element={<ChatJobSeeker />} />
-            <Route path="/notification" element={<NotificationPage />} />
-            <Route path="/findJobs" element={<JobSearch />} />
-            <Route path="/feeds" element={<Dashboard />} />
-            <Route element={<RecruiterRoute />}>
-              <Route path="/jobPosting" element={<JobPostingForm />} />
-              <Route path="/applications" element={<ApplicationsPage />} />
+      <Layout>
+        <main className="w-full min-h-screen flex flex-col bg-neutral-50 text-neutral-500">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<SignUpPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/editProfile" element={<EditProfile />} />
+              <Route path="/chat" element={<Chat />} />
+              <Route path="/chatJob" element={<ChatJobSeeker />} />
+              <Route path="/notification" element={<NotificationPage />} />
+              <Route path="/findJobs" element={<JobSearch />} />
+              <Route path="/feeds" element={<Dashboard />} />
+              <Route element={<RecruiterRoute />}>
+                <Route path="/jobPosting" element={<JobPostingForm />} />
+                <Route path="/applications" element={<ApplicationsPage />} />
+              </Route>
             </Route>
-          </Route>
-        </Routes>
-        <Footer />
-      </main>
+          </Routes>
+        </main>
+      </Layout>
     </Router>
   );
 }
