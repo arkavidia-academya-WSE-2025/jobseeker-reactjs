adfe6359d10b20d566eafaf53d441c5f6ff9194d
xturus138 <radityaramadhan.138@gmail.com>
2025-03-21 14:12:40 +0700

    feat: Added Profile component and removed ProfileDetails and ProfileEdit components

---
+++ fe-arkavidia/src/components/lib/axios.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "http://127.0.0.1:3000",
+  timeout: 1000,
+});
+
+// Optional: Add a method for making GET requests
+export const fetchData = async (endpoint) => {
+  try {
+    const response = await apiClient.get(endpoint);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export default apiClient;
